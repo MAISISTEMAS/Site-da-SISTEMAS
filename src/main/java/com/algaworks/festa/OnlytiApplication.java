51b9189ae033434434eab17b1a8fb78abf5668c7
CleutonJorge <juniorcj_14@hotmail.com>
2019-05-01 20:26:53 -0300

    Eviando Projeto do site para o git

---
+++ src/main/java/com/algaworks/festa/OnlytiApplication.java
@@ -0,0 +1,14 @@
+package com.algaworks.festa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlytiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlytiApplication.class, args);
+	}
+
+}
+
